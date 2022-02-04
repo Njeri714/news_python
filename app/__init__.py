19b9b4f1655fdd9374db8fbd94ca00f794943a31
njeri waweru <wawerunjeri714@gmail.com>
2022-02-04 09:12:36 +0300

    added content in app/__init__.py

---
+++ app/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from .config import DevConfig
+
+#  initializing app
+app = Flask(__name__, instance_relative_config=True)
+
+# setting configarations
+app.config.from_objects(DevConfig)
+
+from app import views
