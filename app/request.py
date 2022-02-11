ddd83ceb3b91fd5fef1306a2851761f5b31f2c70
njeri waweru <wawerunjeri714@gmail.com>
2022-02-11 11:04:09 +0300

    updates

--- app/request.py
+++ app/request.py
@@ -1,3 +1,4 @@
+from urllib.request import Request
 from app import app
 import request
 from .config import Config
@@ -30,3 +31,28 @@ def tech_news():
         tech_object = News_article(id, title, poster, url_link, description, published_date, content)
         tech_news.append(tech_object)
     return tech_news
+
+
+def all_articles_news():
+    '''
+    all_articles_news function displays all news from various sources without specification
+    '''
+    base_url = all_article_base_url.format(Api_key)
+    all_article_data = Request.get(base_url).json()
+    all_articles = []
+    data_articls = all_article_data.get('articles')
+    print(type(data_articls),all_article_data)
+    for articles_data in all_article_data['articles'] :
+        id = articles_data.get('source')
+        title = articles_data.get('title')
+        poster = articles_data.get('urlToImage')
+        url_link = articles_data.get('url')
+        description = articles_data.get('description')
+        published_date = articles_data.get('publishedAt')
+        content = articles_data.get('content')
+        
+        articles_object = News_article(id, title, poster, url_link, description, published_date, content)
+        all_articles.append(articles_object)
+
+    return all_articles
+
