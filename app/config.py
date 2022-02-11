4ac71ff55d2b64cdcaa8d1a4c98c7e4372d85eb9
njeri waweru <wawerunjeri714@gmail.com>
2022-02-11 10:59:46 +0300

    erased virtual and installed again added content

--- app/config.py
+++ app/config.py
@@ -2,4 +2,20 @@ class Config:
     '''
     parent class for all config and sub classes
     '''
+    ALL_ARTICLES_BASE_URL="https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey={}"
+    TECH_CRUNCH_BASE_URL = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey={}"
+    BUSINESS_BASE_URL = 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey={}'
+    TESLA_BASE_URL = "https://newsapi.org/v2/everything?q=tesla&from=2021-12-29&sortBy=publishedAt&apiKey={}"
+    APPLE_BASE_URL = 'https://newsapi.org/v2/everything?q=apple&from=2022-01-28&to=2022-01-28&sortBy=popularity&apiKey={}'
+
+class ProdConfig(Config):
+    '''
+    child of the Config class
+    '''
+    pass
+class DevConfig(Config):
+    '''
+    child of the Config class
+    '''
+    DEBUG = True
     
---
+++ manage.py
@@ -0,0 +1,7 @@
+import request
+
+from distutils.log import DEBUG
+from app import app
+
+if __name__ == '__main__':
+    app.run
