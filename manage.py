c2d5d62f6abf784443d3e0546d83b4c6be50c43b
njeri waweru <wawerunjeri714@gmail.com>
2022-02-16 09:31:12 +0300

    added content

--- manage.py
+++
@@ -1,5 +0,0 @@
-from distutils.log import DEBUG
-from app import app
-
-if __name__ == '__main__':
-    app.run
---
+++ run.py
@@ -0,0 +1,6 @@
+# import requests
+from distutils.log import debug
+from app import app
+
+if __name__ == '__main__':
+    app.run()
